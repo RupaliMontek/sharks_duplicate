8039644b783b9b182c06ec3560c9defca4d61851
ShwetaSB0726 <shwetabhilaressb558@gmail.com>
2025-01-09 12:55:46 +0530

    gitignore commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sharksjob.com/';
+$config['base_url'] = 'https://localhost/sharks_job/';
 // error_reporting(E_ALL & ~E_DEPRECATED);
 
 /*
